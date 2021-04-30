07f01d6f105f9e445e74f2b265b69388b28e3785
Daniel Doyle <tr33m4n@googlemail.com>
2021-04-30 20:31:00 +0100

    WIP: Basic module

---
+++ src/Exception/UrlException.php
@@ -0,0 +1,17 @@
+<?php
+
+declare(strict_types=1);
+
+namespace tr33m4n\UrlObject\Exception;
+
+use Exception;
+
+/**
+ * Class UrlException
+ *
+ * @package tr33m4n\UrlObject\Exception
+ */
+class UrlException extends Exception
+{
+    //
+}
